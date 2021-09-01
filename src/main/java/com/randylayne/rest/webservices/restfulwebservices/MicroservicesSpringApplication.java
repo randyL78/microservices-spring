88ddbc155ee9ee7a42f6c6673c1f4d039cff66fc
Randy Layne <randy.layne@teamtreehouse.com>
2021-09-01 12:14:21 -0400

    Initial commit

---
+++ src/main/java/com/randylayne/rest/webservices/restfulwebservices/MicroservicesSpringApplication.java
@@ -0,0 +1,13 @@
+package com.randylayne.rest.webservices.restfulwebservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroservicesSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/randylayne/rest/webservices/restfulwebservices/MicroservicesSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.randylayne.rest.webservices.restfulwebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
